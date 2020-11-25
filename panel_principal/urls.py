b164079433d8112c3de7feacf52318adfeb74817
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2020-11-25 03:41:06 -0300

    Se agrega la creacion de usuarios, login, pagina inicial welcome, css y se agrega paginas base para tener plantillas reutilizables

--- panel_principal/urls.py
+++ panel_principal/urls.py
@@ -1,7 +1,12 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.horas_list, name='horas_list'),
-    #path('index', name='index.html'),
+    path('index',views.pantalla_plana, name='index.html'),
+    path('welcome', views.welcome),
+    path('register', views.register),
+    path('login', views.login),
+    path('logout', views.logout),
 ]
--- panel_principal/views.py
+++ panel_principal/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
+from django.contrib.auth import logout as do_logout
+from django.contrib.auth import authenticate, login as dj_login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import Boxe, Paciente, Medico, Notificacione, Horas_medica, Llamada_medica
+
+
 
 
 # Create your views here.
@@ -8,8 +14,78 @@ def horas_list(request):
     llamadas = Llamada_medica.objects.filter().order_by('created_date')
     return render(request, 'panel_principal/templates/horas_list.html', {'llamadas': llamadas})
 
-"""
+
 def pantalla_plana(request):
     return render(request, 'index.html',{})
 
-"""
+
+
+def welcome(request):
+    # Si estamos identificados devolvemos la portada
+    if request.user.is_authenticated:
+        return render(request, "welcome.html")
+    # En otro caso redireccionamos al login
+    return redirect('/login')
+
+def register(request):
+    # Creamos el formulario de autenticación vacío
+    
+    form = UserCreationForm()
+
+    form.fields['username'].help_text = None
+    form.fields['password1'].help_text = None
+    form.fields['password2'].help_text = None
+
+    # Si llegamos al final renderizamos el formulario
+    
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = UserCreationForm(data=request.POST)
+        # Si el formulario es válido...
+        if form.is_valid():
+
+            # Creamos la nueva cuenta de usuario
+            user = form.save()
+
+            # Si el usuario se crea correctamente 
+            if user is not None:
+                # Hacemos el login manualmente
+                dj_login(request, user)
+                # Y le redireccionamos a la portada
+                return redirect('/')
+
+    # Si llegamos al final renderizamos el formulario
+    return render(request, "register.html", {'form': form})
+
+def login(request):
+    error_message = None
+    heading = 'Login Form'
+    # Creamos el formulario de autenticación vacío
+    form = AuthenticationForm()
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = AuthenticationForm(data=request.POST)
+        # Si el formulario es válido...
+        if form.is_valid():
+            # Recuperamos las credenciales validadas
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # Verificamos las credenciales del usuario
+            user = authenticate(username=username, password=password)
+
+            # Si existe un usuario con ese nombre y contraseña
+            if user is not None:
+                # Hacemos el login manualmente
+                dj_login(request, user)
+                # Y le redireccionamos a la portada
+                return redirect('/welcome')
+
+    # Si llegamos al final renderizamos el formulario
+    return render(request, "login.html", {'form': form})
+
+def logout(request):
+    # Finalizamos la sesión
+    do_logout(request)
+    # Redireccionamos a la portada
+    return redirect('/')
